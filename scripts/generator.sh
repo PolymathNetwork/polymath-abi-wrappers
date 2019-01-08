10923ffaedbdd29d05cf05e27f6515ea40d9e1e5
shuffledex <shuffledex@gmail.com>
2019-01-08 14:13:33 -0300

    ts generated from polymath-contract-artifacts#v2.1.0-beta

---
+++ scripts/generator.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# params validation
+if [[ -z "$1" ]]; then
+  echo "Tag is empty"
+  exit 1
+fi
+
+yarn upgrade polymath-contract-artifacts@git://github.com/PolymathNetwork/polymath-contract-artifacts.git#$1
+yarn pre_build && yarn build
+git add --all && git commit -m "ts generated from polymath-contract-artifacts#$1" && git tag -a $1 -m "automatic release from polymath-contract-artifacts#$1" && git push origin $1
+
+echo "Typescripts successfully created..."
